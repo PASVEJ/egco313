5693d8be0b73b87927e34934c436d36a86febd9a
PASVEJ <31722925+PASVEJ@users.noreply.github.com>
2017-11-30 16:27:16 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+	<title>Hello world!</title>
+	<style>
+	body {
+		background-color: white;
+		text-align: center;
+		padding: 50px;
+		font-family: "Open Sans","Helvetica Neue",Helvetica,Arial,sans-serif;
+	}
+
+	#logo {
+		margin-bottom: 40px;
+	}
+	</style>
+</head>
+<body>
+	<img id="logo" src="logo.png" />
+	<h1>Hello world!</h1>
+			<h2>MySQL Server version: 5.5.47-0ubuntu0.14.04.1</h2>
+	</body>
+</html>
